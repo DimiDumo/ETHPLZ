91f683dfda457ca246fdcbaecc8d93836a38ad6b
Alexander Potapov <al.potapov@hotmail.com>
2022-01-15 00:04:32 +0100

    Fix the import issue

--- frontend/src/pages/BasePage.jsx
+++ frontend/src/pages/BasePage.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Layout from 'antd/lib/layout/layout';
 
-const { Header, Content } = Layout;
+import { Layout } from 'antd';
+
+const { Header, Footer, Content } = Layout;
 
 const BasePage = ({ children }) => {
   return (
     <Layout>
       <Header>Header</Header>
       <Content>{children}</Content>
+      <Footer>Footer</Footer>
     </Layout>
   );
 };
