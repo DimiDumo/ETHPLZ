3e4e1ad9058237132703a3de5e4c7762ba352170
Alexander Potapov <al.potapov@hotmail.com>
2022-01-15 03:21:16 +0100

    Use tailwind and daisyui instead of the ant design

--- frontend/src/pages/Discovery.jsx
+++ frontend/src/pages/Discovery.jsx
@@ -1,17 +1,21 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
-
-import { Skeleton } from 'antd';
+import Holder from 'holderjs';
 
 import BasePage from './BasePage/BasePage';
 
 import './Discovery.scss';
 
 const Discovery = () => {
-  const content = [{}, {}, {}, {}, {}, {}].map((item) => ({
+  const content = [{}, {}, {}, {}, {}, {}, {}].map((item) => ({
     id: uuidv4(),
     ...item,
   }));
+
+  React.useEffect(() => {
+    Holder.run();
+  }, []);
 
   const handleClick = (id) => {
     console.log(`clicked ${id}`);
@@ -21,11 +25,15 @@ const Discovery = () => {
       <div className="Discovery-Title">
         <h1>Discover</h1>
       </div>
-      <div className="Discovery-Content">
+      <div className="grid grid-cols-3 gap-4 p-2 pt-4 bg-base-100">
         {content.map((item) => (
-          <div key={item.id} role="button" onClick={() => handleClick(item.id)}>
-            <Skeleton.Image />
-          </div>
+          <span
+            key={item.id}
+            role="button"
+            onClick={() => handleClick(item.id)}
+          >
+            <img src="holder.js/300x200" alt="" />
+          </span>
         ))}
       </div>
     </BasePage>
