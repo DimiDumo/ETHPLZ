75b161cdb35214a37db14da3699299b64d408339
philogy <philippe@dumo.net>
2022-01-15 19:58:17 +0100

    Allow faster response

--- web3-master-service/src/routes.js
+++ web3-master-service/src/routes.js
@@ -2,16 +2,16 @@ const express = require('express')
 const router = express.Router()
 const { createHandler } = require('./api.js')
 const { ethers } = require('ethers')
-const { getMasterWallet } = require('./provider.js')
+const getMasterWallet = require('./provider.js')
 const { ApiError } = require('./api.js')
 const PleaseWalletFactory = require('../contracts/WalletFactory.json')
 
 const getContract = (provider, { abi, address }) => new ethers.Contract(address, abi, provider)
 
 router.post(
-  '/create-new-wallet/:primaryKey',
+  '/create-new-wallet',
   createHandler(async ({ body }) => {
-    let { primaryKey } = body
+    let { primaryKey, provideNewWalletAddress = false } = body
     if (!ethers.utils.isAddress(primaryKey)) {
       throw new ApiError(400, `"${primaryKey}" is not a valid address`)
     }
@@ -19,6 +19,9 @@ router.post(
     const { masterWallet } = getMasterWallet()
     const walletFactory = getContract(masterWallet, PleaseWalletFactory)
     const tx = await walletFactory.createDefaultWallet(primaryKey)
+    if (!provideNewWalletAddress) {
+      return { newWalletAddress: null }
+    }
     const { events } = await tx.wait()
     const [newWalletCreation] = events.filter(({ event }) => event === 'NewWalletCreated')
 
