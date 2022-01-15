634fd092485ccf912e1b4047cc75579304ff2ec4
philogy <philippe@dumo.net>
2022-01-15 22:28:34 +0100

    Remove expensive OZ StorageSlot

--- contracts/contracts/SimpleWalletProxy.sol
+++ contracts/contracts/SimpleWalletProxy.sol
@@ -2,24 +2,25 @@
 pragma solidity ^0.8.11;
 
 import "@openzeppelin/contracts/proxy/Proxy.sol";
-import "@openzeppelin/contracts/utils/StorageSlot.sol";
 
 contract SimpleWalletProxy is Proxy {
-    using StorageSlot for bytes32;
-
     // keccak256("eip1967.proxy.implementation") - 1
     bytes32 internal constant _IMPLEMENTATION_SLOT =
         0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
 
     constructor(address _initialImplementation) {
-        _IMPLEMENTATION_SLOT.getAddressSlot().value = _initialImplementation;
+        assembly {
+            sstore(_IMPLEMENTATION_SLOT, _initialImplementation)
+        }
     }
 
     function getImplementation() external view returns (address) {
         return _implementation();
     }
 
-    function _implementation() internal view override returns (address) {
-        return _IMPLEMENTATION_SLOT.getAddressSlot().value;
+    function _implementation() internal view override returns (address implementation) {
+        assembly {
+            implementation := sload(_IMPLEMENTATION_SLOT)
+        }
     }
 }
