fb3202de2274647b644ff0e158e82893cb3ca024
Alexander Potapov <al.potapov@hotmail.com>
2022-01-15 02:59:53 +0100

    Show placeholder images on Discover page

--- frontend/src/pages/Discovery.jsx
+++ frontend/src/pages/Discovery.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+import { Skeleton } from 'antd';
+
 import BasePage from './BasePage/BasePage';
 
+import './Discovery.scss';
+
 const Discovery = () => {
+  const content = [{}, {}, {}, {}, {}, {}].map((item) => ({
+    id: uuidv4(),
+    ...item,
+  }));
+
+  const handleClick = (id) => {
+    console.log(`clicked ${id}`);
+  };
   return (
     <BasePage>
-      <div>Discovery</div>
+      <div className="Discovery-Title">
+        <h1>Discover</h1>
+      </div>
+      <div className="Discovery-Content">
+        {content.map((item) => (
+          <div key={item.id} role="button" onClick={() => handleClick(item.id)}>
+            <Skeleton.Image />
+          </div>
+        ))}
+      </div>
     </BasePage>
   );
 };
