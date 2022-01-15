b9eed50bd5558c665001192786dc543465a53442
philogy <philippe@dumo.net>
2022-01-15 20:09:21 +0100

    Remove unused / invalid code

--- backend/cloud/cloud.js
+++ backend/cloud/cloud.js
@@ -2,14 +2,10 @@ Moralis.Cloud.define(
   'createNewUserWallet',
   async ({ params: primaryKey, user }) => {
     const logger = Moralis.Cloud.getLogger()
-    const web3 = Moralis.web3ByChain('0x4')
-    const { address: mainAccount } = web3.eth.accounts.wallet.add(CLOUD_ENV().MASTER_KEY)
 
     if (user.localWalletAddress !== '0x0000000000000000000000000000000000000000') {
       throw new Error('Wallet already registered')
     }
-
-    const contracts = getContracts(web3)
 
     return { wow: 'bob' }
   },
