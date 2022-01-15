1ceef8571d6bfb3daf38dda1474bacdeb40340df
Alexander Potapov <al.potapov@hotmail.com>
2022-01-15 12:10:55 +0100

    eslint

--- frontend/src/pages/BasePage/BasePage.jsx
+++ frontend/src/pages/BasePage/BasePage.jsx
@@ -10,20 +10,16 @@ const BasePage = (props) => {
   return (
     <div className="flex h-screen flex-col">
       <header className="BasePage-Header fixed">
-        <div className="phone-notch"/>
-          <p className="header-title">{headerTitle}</p>
+        <div className="phone-notch" />
+        <p className="header-title">{headerTitle}</p>
       </header>
       <div className="container content h-10 flex-grow">
         {children}
-        <div className="spacer"/>
+        <div className="spacer" />
       </div>
       <footer className="container fixed bottom-0 mx-auto flex flex-row justify-around">
         <BottomLink path="/" label="Discover" icon={LightBulbIcon} />
-        <BottomLink
-          path="/portfolio"
-          label="Portfolio"
-          icon={BookOpenIcon}
-        />
+        <BottomLink path="/portfolio" label="Portfolio" icon={BookOpenIcon} />
         {/* <BottomLink path="/profile" label="Profile" icon={AdjustmentsIcon} /> */}
         <BottomLink path="/settings" label="Settings" icon={CogIcon} />
       </footer>
@@ -33,7 +29,7 @@ const BasePage = (props) => {
 
 BasePage.propTypes = {
   children: PropTypes.node.isRequired,
-  headerTitle: PropTypes.string.isRequired
+  headerTitle: PropTypes.string.isRequired,
 };
 
 export default BasePage;
