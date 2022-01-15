79b32ef378adfdd343411226b3c693bc3beefe43
philogy <philippe@dumo.net>
2022-01-15 12:12:25 +0100

    Remove unused interface

--- contracts/contracts/ISelfUpgradeable.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-only
-pragma solidity ^0.8.11;
-
-interface ISelfUpgradeable {
-    function upgradeSelfTo(address _newImplementation, bytes calldata _initCallData) external;
-}
