04ddb68ca2fc3027f162608e62cd42bb81ae346e
Dimitri Dumonet <dimitri@emax-digital.com>
2022-01-15 10:32:52 +0100

    Add settings skelleton to settings page

--- frontend/src/pages/Settings/Settings.jsx
+++ frontend/src/pages/Settings/Settings.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import BasePage from '../BasePage/BasePage';
 
 const Settings = () => {
+
   return (
     <BasePage headerTitle='Settings'>
-      <div>Settings</div>
+      <div className="grid gap-1 p-3 pt-4">
+        <div className='settings-item'>Profile</div>
+        <div className='settings-item'>Preference</div>
+        <div className='settings-item'>Payment Details</div>
+        <div className='settings-item'>History</div>
+        <div className='settings-item'>Contact Support</div>
+      </div>
     </BasePage>
   );
 };
